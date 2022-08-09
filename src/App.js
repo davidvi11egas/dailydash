6bc3c99fcdf67e4ae2c1ca2a3146936ffdf62a25
David Villegas - Next Horizon <DVillegas@nexhorizon.net>
2022-08-09 11:53:28 -0400

    implemented protected route to home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import { UserAuthContextProvider } from "./context/userAuthContext";
+import ProtectedRoute from "./context/ProtectedRoute";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -13,7 +14,14 @@ function App() {
         <Col>
           <UserAuthContextProvider>
             <Routes>
-              <Route path="/home" element={<Home />} />
+            <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
               <Route path="/" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
             </Routes>
--- src/context/ProtectedRoute.js
+++ src/context/ProtectedRoute.js
@@ -1,6 +1,8 @@
+// This function is to ensure pages are only shown to users who are logged in.
+// the page must be wrapped within the protectedRoute tag on the App.js page to protect it.
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useUserAuth } from "../context/userAuthContext";
+import { useUserAuth } from "./userAuthContext";
 const ProtectedRoute = ({ children }) => {
   const { user } = useUserAuth();
 
